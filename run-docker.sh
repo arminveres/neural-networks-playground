5c53d72c34e506cca5c6df0f68732591c32458df
Armin Veres <armin.veres@hotmail.com>
2023-05-01 20:47:04 +0200

    added webcam to HIP docker

--- run-docker.sh
+++ run-docker.sh
@@ -2,6 +2,6 @@
 
 set -ev
 
-docker run -it --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --device=/dev/kfd --device=/dev/dri --group-add video --ipc=host --shm-size 8G \
+docker run -it --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --device=/dev/kfd --device=/dev/dri --device /dev/video0 --group-add video --ipc=host --shm-size 8G \
     -v ./:/neural-networks\
     rocm/pytorch:latest

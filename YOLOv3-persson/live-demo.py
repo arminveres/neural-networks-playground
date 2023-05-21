ec89c8d914df4033298763d5edd60572bfd97ccd
Armin Veres <armin.veres@hotmail.com>
2023-05-21 14:44:37 +0200

    feat: stream camera to url

--- YOLOv3-persson/live-demo.py
+++ YOLOv3-persson/live-demo.py
@@ -5,6 +5,9 @@ import config
 import cv2
 import numpy as np
 import time
+import urllib.request
+
+url = "http://127.0.0.1:5000/video_feed"
 
 
 def prep_image(img, inp_dim):
@@ -89,54 +92,70 @@ def main():
 
     frames = 0
     start = time.time()
-    webcam = cv2.VideoCapture(0)  # try different number if not working
-    assert webcam.isOpened()
+    # webcam = cv2.VideoCapture(0)  # try different number if not working
+    # assert webcam.isOpened()
+
+    stream = urllib.request.urlopen(url)
+    bytes_buffer = b""
 
     while True:
         frames += 1
-        check, test_image = webcam.read()
-        if not check:
-            break
-        # Exit the loop if the user presses the 'q' key
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
 
-        if frames % 10 == 0:
-            print("FPS: {:5.2f}".format(frames / (time.time() - start)))
+        # check, test_image = webcam.read()
+        # if not check:
+        #     break
 
-        input_image = prep_image(test_image, config.IMAGE_SIZE)
+        bytes_buffer += stream.read(1024)
+        a = bytes_buffer.find(b"\xff\xd8")
+        b = bytes_buffer.find(b"\xff\xd9")
+        if a != -1 and b != -1:
+            jpg = bytes_buffer[a : b + 2]
+            bytes_buffer = bytes_buffer[b + 2 :]
 
-        scaled_anchors = (
-            torch.tensor(config.ANCHORS)
-            * torch.tensor(config.S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
-        ).to(config.DEVICE)
-
-        # convert image to GPU
-        input_image = input_image.to(config.DEVICE)
-
-        with torch.no_grad():
-            out = model(input_image)
-            bboxes = [[] for _ in range(input_image.shape[0])]
-            for i in range(3):
-                batch_size, _, S, _, _ = out[i].shape
-                anchor = scaled_anchors[i]
-                boxes_scale_i = cells_to_bboxes(out[i], anchor, S=S, is_preds=True)
-                for idx, (box) in enumerate(boxes_scale_i):
-                    bboxes[idx] += box
-
-        for i in range(batch_size):
-            nms_boxes = non_max_suppression(
-                bboxes[i],
-                iou_threshold=0.6,
-                threshold=0.5,
-                box_format="midpoint",
+            test_image = cv2.imdecode(
+                np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR
             )
-        # for i in range(batch_size):
-        add_boxes_to_image(test_image, nms_boxes)
 
-        cv2.imshow("final output", test_image)
+            # Exit the loop if the user presses the 'q' key
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
 
-    webcam.release()
+            if frames % 10 == 0:
+                print("FPS: {:5.2f}".format(frames / (time.time() - start)))
+
+            input_image = prep_image(test_image, config.IMAGE_SIZE)
+
+            scaled_anchors = (
+                torch.tensor(config.ANCHORS)
+                * torch.tensor(config.S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
+            ).to(config.DEVICE)
+
+            # convert image to GPU
+            input_image = input_image.to(config.DEVICE)
+
+            with torch.no_grad():
+                out = model(input_image)
+                bboxes = [[] for _ in range(input_image.shape[0])]
+                for i in range(3):
+                    batch_size, _, S, _, _ = out[i].shape
+                    anchor = scaled_anchors[i]
+                    boxes_scale_i = cells_to_bboxes(out[i], anchor, S=S, is_preds=True)
+                    for idx, (box) in enumerate(boxes_scale_i):
+                        bboxes[idx] += box
+
+            for i in range(batch_size):
+                nms_boxes = non_max_suppression(
+                    bboxes[i],
+                    iou_threshold=0.6,
+                    threshold=0.5,
+                    box_format="midpoint",
+                )
+            # for i in range(batch_size):
+            add_boxes_to_image(test_image, nms_boxes)
+
+            cv2.imshow("final output", test_image)
+
+    # webcam.release()
     cv2.destroyAllWindows()
     return
 
